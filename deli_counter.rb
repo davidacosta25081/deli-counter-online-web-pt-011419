2656db21fb88e0970bf12e962c86c6e00a7fe871
David Acosta <davidacosta25081@yahoo.com>
2019-01-30 07:22:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line (costumers)
+if costumers.empty
+  puts "The line is currently empty."
+else
+  costumers.each_with_index do |costumer,index|
+      puts "The line is currently #{index}.#{costumer }"
+    end
+  end
+end
+  
