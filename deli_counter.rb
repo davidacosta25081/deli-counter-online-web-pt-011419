e3b02bc8eb8f87b89b4d3e2fad68401fb7dad51d
David Acosta <davidacosta25081@yahoo.com>
2019-01-30 07:42:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ new_array = []
 if costumers.empty?
   puts "The line is currently empty."
 else
-  costumers.each_with_index(1) do |costumer,index|
+  costumers.each.with_index(1) do |costumer,index|
       new_array.push("#{index}. #{name}")  #{index}.#{costumer }"
     end
 puts "The line is currently: #{new_array.join(" ")}"
